1acb348548afaa3b42fef8362b7819edf119e3b9
Shane Poppleton <shane@alphasg.com.au>
2019-10-20 15:52:40 +1000

    First Commit

---
+++ resources/js/app.js
@@ -0,0 +1,22 @@
+import VueAutoDropzone from "vue-auto-dropzone";
+
+require('./bootstrap');
+import Vue from 'vue';
+
+
+window.Vue = Vue;
+window.events = new Vue();
+
+Vue.component('vue-auto-dropzone', VueAutoDropzone);
+const app = new Vue({
+    el: '#app',
+    data: {
+    options: {
+        url: '/admin/api/images',
+        autoProcessQueue: false,
+        resizeHeight: 777,
+        resizeWidth: 1920,
+        acceptedFiles: 'image/*',
+        maxFiles: 1
+    }}
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <vue-auto-dropzone :options="options"></vue-auto-dropzone>
+@endsection
